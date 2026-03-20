eaec5283cc599d13866c8d01b6ace3c6f5f1c744
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-03-20 11:22:30 -0500

    Some config file stuff thast almost done thats aboslutely a mess but hwo cares, commiting just because I'm doing stuff that I may need to revrt

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,23 +1,49 @@
 use thirtyfour::prelude::*;
-use std::{error::Error, path::Path};
+use whoami::platform;
+use std::{env, error::Error, fs::{self, create_dir_all}, path::{Path, PathBuf}};
 use ftail::Ftail;
 use log::LevelFilter;
-use std::fs::File;
 pub mod make_config_file;
+use directories::BaseDirs;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    Ftail::new()
-    .console(LevelFilter::Info)
-    .daily_file(Path::new("../../logs"), LevelFilter::Info)
-    .init()?;
+    if let Some(base_dirs) = BaseDirs::new() {
+        let local_data_directory = base_dirs.data_local_dir();
+        let os = platform().to_string();
 
-    if ! Path::exists(Path::new("../../configs/")) {
-        log::info!("User had no config file, starting the creation process.");
-        make_config_file::make_config_file()
+        // Set to right directory based off linux or windows
+        let log_directory = if os == "Windows" {
+            local_data_directory.join("slightly_deranged_lunatic\\discord_bruteforcer")
+        } else {
+            local_data_directory.join("slightly_deranged_lunatic/discord_bruteforcer")
+        };
+        println!("{}", local_data_directory.display());
+        // Make log directory
+        if ! Path::exists(local_data_directory) {
+            fs::create_dir_all(local_data_directory)?;
+        }
+        // Initalize logs
+        Ftail::new()
+        .console(LevelFilter::Info)
+        .daily_file(&log_directory, LevelFilter::Info)
+        .init()?;
+
+
+        // Does a config file exist?
+        let local_config_dir = base_dirs.config_local_dir();
+        let local_config_dir = if os == "Windows" {
+            local_config_dir.join("Slightly_deranged_lunatic\\discord_bruteforcer")
+        } else {
+            local_config_dir.join("Slightly_deranged_lunatic//discord_bruteforcer")
+        };
+        if ! Path::exists(&local_config_dir) {
+            log::info!("User had no config file, starting the creation process.");
+            make_config_file::make_config_file()
+        }
+
     }
-
     let caps = DesiredCapabilities::firefox();
     let driver = WebDriver::new("http://localhost:4444", caps).await?;
     driver.goto("https://discord.com/login").await?;
--- main/src/make_config_file/mod.rs
+++ main/src/make_config_file/mod.rs
@@ -1,5 +1,9 @@
 use std::io;
 use std::path::Path;
+use std::env;
+use ftail::Ftail;
+use log::LevelFilter;
+use directories::BaseDirs;
 
 struct ConfigValues {
     email: String,
@@ -7,8 +11,15 @@ struct ConfigValues {
     new_email: String
 }
 
+
 pub fn make_config_file() {
+    Ftail::new()
+    .console(LevelFilter::Info)
+    .daily_file(Path::new("../../logs"), LevelFilter::Info)
+    .init();
     let user_config_values = get_config_values();
+    create_config_file();
+
 }
 
 fn get_config_values() -> ConfigValues {
@@ -21,7 +32,7 @@ fn get_config_values() -> ConfigValues {
         .expect("Failed to read line");
     clearscreen::clear().expect("failed to clear screen");
 
-    println!("What is your password? Please note this WILL be stored in plaintext, however, should you be uncomfortable with this, you can put it in yourself at the login screen.");
+    println!("What is your password? Please note this WILL be stored in plaintext, however, shouldn't you be uncomfortable with this, you can put it in yourself at the login screen.");
     let mut password = String::new();
     io::stdin()
         .read_line(& mut password)
@@ -48,7 +59,14 @@ fn get_config_values() -> ConfigValues {
     return user_config_values;
 }
 
-fn create_config_file() {
+fn create_config_file() -> io::Result<()> {
     // Function that actually makes the configuration file, here because where it is depends on the OS and stuff for best practice
-    let username = whoami::username_os(); // I'm not going to lie I don't even know if this the right thing to get but uh yeah
+
+    // Get users config file location
+    if let Some(base_dirs) = BaseDirs::new() {
+        let local_config = base_dirs.config_local_dir();
+        env::set_current_dir(local_config);
+        log::info!("Set current directory to {}.", local_config.display());
+    }
+    Ok(())
 }
