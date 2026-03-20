8e4b2112a98bed1e29690570c8634c0154a32f82
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-03-20 04:34:15 -0500

    Navigate to url and setup logging

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,10 +1,20 @@
 use thirtyfour::prelude::*;
-use std::error::Error;
+use std::{error::Error, path::Path};
+use ftail::Ftail;
+use log::LevelFilter;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    Ftail::new()
+    .console(LevelFilter::Info)
+    .daily_file(Path::new("../../logs"), LevelFilter::Error)
+    .init()?;
+
     let caps = DesiredCapabilities::firefox();
     let driver = WebDriver::new("http://localhost:4444", caps).await?;
+    driver.goto("https://discord.com/login").await?;
+    log::info!("Opened and navigated to https://discord.com/login");
 
+    driver.quit().await?;
     Ok(())
 }
