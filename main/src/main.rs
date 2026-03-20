1ad3d759816b37180c7ba3bae748f5d120bcb6d8
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-03-20 04:05:28 -0500

    Add tokio dependcy and do some weird expirmental stuff that doesn't do much

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use thirtyfour::prelude::*;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let caps = DesiredCapabilities::firefox();
+    let driver = WebDriver::new("http://localhost:4444", caps).await?;
+
+    Ok(())
 }
